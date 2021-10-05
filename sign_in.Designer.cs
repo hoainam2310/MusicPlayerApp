516ce207ef39a2774f85f985f1c6aa654ccef230
minh30009 <91043332+minh30009@users.noreply.github.com>
2021-10-05 19:09:25 +0700

    addfile

--- sign_in.Designer.cs
+++ sign_in.Designer.cs
@@ -30,17 +30,17 @@ namespace MusicPlayerApp
         private void InitializeComponent()
         {
             this.gb_login = new System.Windows.Forms.GroupBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.txt_password = new System.Windows.Forms.TextBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.txt_username = new System.Windows.Forms.TextBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.gb_login.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // gb_login
@@ -58,12 +58,61 @@ namespace MusicPlayerApp
             this.gb_login.TabIndex = 0;
             this.gb_login.TabStop = false;
             // 
+            // txt_password
+            // 
+            this.txt_password.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txt_password.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_password.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_password.ForeColor = System.Drawing.Color.DarkGray;
+            this.txt_password.Location = new System.Drawing.Point(955, 257);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(237, 25);
+            this.txt_password.TabIndex = 4;
+            this.txt_password.Text = "Enter Your Password";
+            this.txt_password.Enter += new System.EventHandler(this.txt_password_Enter);
+            this.txt_password.Leave += new System.EventHandler(this.txt_password_Leave);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::MusicPlayerApp.Properties.Resources.input;
+            this.pictureBox4.Location = new System.Drawing.Point(916, 243);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(295, 51);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // txt_username
+            // 
+            this.txt_username.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txt_username.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_username.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_username.ForeColor = System.Drawing.Color.DarkGray;
+            this.txt_username.Location = new System.Drawing.Point(955, 169);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(237, 25);
+            this.txt_username.TabIndex = 2;
+            this.txt_username.Text = "E-mail Address";
+            this.txt_username.Enter += new System.EventHandler(this.txt_username_Enter);
+            this.txt_username.Leave += new System.EventHandler(this.txt_username_Leave);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::MusicPlayerApp.Properties.Resources.input;
+            this.pictureBox3.Location = new System.Drawing.Point(916, 155);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(295, 51);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
             // pictureBox2
             // 
             this.pictureBox2.Image = global::MusicPlayerApp.Properties.Resources.LOGIN;
-            this.pictureBox2.Location = new System.Drawing.Point(875, 360);
+            this.pictureBox2.Location = new System.Drawing.Point(916, 362);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(307, 82);
+            this.pictureBox2.Size = new System.Drawing.Size(295, 82);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 8;
             this.pictureBox2.TabStop = false;
@@ -78,66 +127,21 @@ namespace MusicPlayerApp
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
             // 
-            // txt_password
-            // 
-            this.txt_password.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.txt_password.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txt_password.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txt_password.ForeColor = System.Drawing.Color.DarkGray;
-            this.txt_password.Location = new System.Drawing.Point(955, 257);
-            this.txt_password.Name = "txt_password";
-            this.txt_password.Size = new System.Drawing.Size(237, 25);
-            this.txt_password.TabIndex = 4;
-            this.txt_password.Text = "Type Your Password";
-            // 
-            // txt_username
-            // 
-            this.txt_username.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.txt_username.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txt_username.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txt_username.ForeColor = System.Drawing.Color.DarkGray;
-            this.txt_username.Location = new System.Drawing.Point(955, 169);
-            this.txt_username.Name = "txt_username";
-            this.txt_username.Size = new System.Drawing.Size(237, 25);
-            this.txt_username.TabIndex = 2;
-            this.txt_username.Text = "E-mail Address";
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = global::MusicPlayerApp.Properties.Resources.input;
-            this.pictureBox3.Location = new System.Drawing.Point(916, 157);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(295, 51);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 9;
-            this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.Image = global::MusicPlayerApp.Properties.Resources.input;
-            this.pictureBox4.Location = new System.Drawing.Point(916, 243);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(295, 51);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox4.TabIndex = 10;
-            this.pictureBox4.TabStop = false;
-            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
-            // 
             // sign_in
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1269, 703);
             this.Controls.Add(this.gb_login);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "sign_in";
             this.Text = "Form1";
             this.gb_login.ResumeLayout(false);
             this.gb_login.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
 
         }
--- sign_in.cs
+++ sign_in.cs
@@ -26,5 +26,43 @@ namespace MusicPlayerApp
         {
 
         }
+
+        private void txt_username_Enter(object sender, EventArgs e)
+        {
+            if(txt_username.Text== "E-mail Address")
+            {
+                txt_username.Text = "";
+                txt_username.ForeColor = Color.Black;
+            }
+        }
+
+        private void txt_username_Leave(object sender, EventArgs e)
+        {
+            if (txt_username.Text=="")
+            {
+                txt_username.Text = "E-mail Address";
+                txt_username.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txt_password_Enter(object sender, EventArgs e)
+        {
+            if (txt_password.Text == "Enter Your Password")
+            {
+                txt_password.Text = "";
+                txt_password.ForeColor = Color.Black;
+                txt_password.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txt_password_Leave(object sender, EventArgs e)
+        {
+            if (txt_password.Text == "")
+            {
+                txt_password.Text = "Enter Your Password";
+                txt_password.ForeColor = Color.Silver;
+                txt_password.UseSystemPasswordChar = false;
+            }
+        }
     }
 }
